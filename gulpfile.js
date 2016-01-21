a86eb7f1cb45c1cef54a4d507eac204197d5a7ad
igari <t.igari@leihauoli.com>
2016-01-21 17:06:26 +0900

    refactored

--- gulpfile.js
+++ gulpfile.js
@@ -2,52 +2,56 @@
 
 var buffer = require('buffer');
 var del = require('del');
-var gulp = require('gulp');
-var nodemon = require('gulp-nodemon');
-var gulpJshint = require('gulp-jshint');
-var gulpUglify = require('gulp-uglify');
-var webserver = require('gulp-webserver');
 var map = require('map-stream');
 
-gulp.task('generate', function () {
+var gulp = require('gulp');
+var gulpNodemon = require('gulp-nodemon');
+var gulpJshint = require('gulp-jshint');
+var gulpUglify = require('gulp-uglify');
+var gulpWebserver = require('gulp-webserver');
 
-  var header = new Buffer('javascript:');
+//generate bookmark
+gulp.task('generate-bookmarklet', function () {
 
-  gulp.src('extension/style-validator.js')
-    .pipe(gulpJshint())
-    .pipe(gulpUglify())
-    .pipe(map(function (file, cb) {
-      file.contents = buffer.Buffer.concat([header, file.contents]);
-      cb(null, file);
-    }))
-    .pipe(gulp.dest('bookmarklet'));
+	var header = new Buffer('javascript:');
+
+	gulp.src('extension/style-validator.js')
+		.pipe(gulpJshint())
+		.pipe(gulpUglify())
+		.pipe(map(function (file, cb) {
+			file.contents = buffer.Buffer.concat([header, file.contents]);
+			cb(null, file);
+		}))
+		.pipe(gulp.dest('bookmarklet'));
 });
 
-gulp.task('clean', del.bind(null, './bookmarklet'));
+gulp.task('clean', del.bind(null, 'bookmarklet'));
 
 gulp.task('watch', function () {
-	var watcher = gulp.watch('extension/style-validator.js', ['generate']);
+	var watcher = gulp.watch('extension/style-validator.js', ['generate-bookmarklet']);
 	watcher.on('change', function(event) {
 		console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
 	});
 });
 
-gulp.task('api-start', function () {
-	nodemon({
-		script: 'overriding-api.js',
+//start server
+gulp.task('start-node-server', function () {
+	gulpNodemon({
+		script: 'app.js',
 		ext: 'html js'
 	})
 });
 
-gulp.task('webserver', function () {
+//open browser
+gulp.task('open-browser', function () {
 	gulp
 		.src('../')
 		.pipe(
-			webserver({
+			gulpWebserver({
 				livereload: false,
 				open: 'http://localhost:8000/Style-Validator/page/rules.html'
 			})
 		);
 });
 
-gulp.task('default', ['clean', 'generate', 'watch', 'api-start', 'webserver']);
+gulp.task('default', ['clean', 'generate-bookmarklet', 'watch', 'start-node-server', 'open-browser']);
