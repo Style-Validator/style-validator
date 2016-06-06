a8e151db9176e3776424bd6b9ca18cf911950d5f
igari <igari.takeharu@icloud.com>
2016-06-06 21:34:28 +0900

    commit for testing selenium on amazon linux

--- webdriverio.test.js
+++ webdriverio.test.js
@@ -1,20 +1,27 @@
 var Xvfb = require('xvfb');
 var xvfb = new Xvfb();
 xvfb.startSync();
+console.log('start')
 
 var webdriverio = require('webdriverio');
+console.log('webdriverio')
 var options = {
 	'browserName': 'google-chrome',
 	'chromeOptions': {
 		'binary': '/usr/bin/google-chrome'//AWS
 	}
 };
+console.log('options')
 webdriverio
 	.remote(options)
 	.init()
+	.then(function() {
+		console.log('init')
+	})
 	.url('http://www.google.com')
 	.getTitle().then(function(title) {
 		console.log('Title was: ' + title);
+		console.log('stop')
 		xvfb.stopSync();
 	})
 	.end();
