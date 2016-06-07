1315b5f687704e85f5f3d9179748a575c28a0a59
igari <igari.takeharu@icloud.com>
2016-06-07 22:28:03 +0900

    commit for testing selenium on amazon linux

--- webdriverio.test.js
+++ webdriverio.test.js
@@ -1,42 +1,18 @@
-//var Xvfb = require('xvfb');
-//var xvfb = new Xvfb();
-//xvfb.startSync();
-//console.log('start')
-var selenium = require('selenium-standalone');
-
-
-selenium.install({
-	drivers: {
-		chrome: {
-			version: '2.9',
-			arch: process.arch,
-			baseURL: 'https://chromedriver.storage.googleapis.com'
-		}
+var webdriverio = require('webdriverio');
+console.log('webdriverio')
+var options = {
+	'browserName': 'google-chrome',
+	'chromeOptions': {
+		'binary': '/usr/bin/google-chrome'//AWS
 	}
-}, function() {
-	console.log('Selenium is installed.');
-	selenium.start(function() {
-		console.log('Selenium is running.');
-
-		var webdriverio = require('webdriverio');
-		console.log('webdriverio')
-		var options = {
-			'browserName': 'google-chrome',
-			'chromeOptions': {
-				'binary': '/usr/bin/google-chrome'//AWS
-			}
-		};
-		console.log('options')
-		webdriverio
-			.remote(options)
-			.init()
-			.url('http://www.google.com')
-			.getTitle().then(function(title) {
-				console.log('Title was: ' + title);
-				console.log('stop')
-				//xvfb.stopSync();
-			})
-			.end();
-	});
-});
-
+};
+console.log('options')
+webdriverio
+	.remote(options)
+	.init()
+	.url('http://www.google.com')
+	.getTitle().then(function(title) {
+		console.log('Title was: ' + title);
+		//xvfb.stopSync();
+	})
+	.end();
