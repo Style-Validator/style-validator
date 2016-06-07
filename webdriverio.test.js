8cfdeb6fcf88df9af6d848097cd68785dc4abdf6
igari <igari.takeharu@icloud.com>
2016-06-08 08:56:42 +0900

    commit for testing selenium on amazon linux

--- webdriverio.test.js
+++ webdriverio.test.js
@@ -1,18 +1,14 @@
 var webdriverio = require('webdriverio');
-console.log('webdriverio')
-var options = {
-	'browserName': 'chrome'
-};
-try {
-	webdriverio
-		.remote(options)
-		.init()
-		.url('http://www.google.com')
-		.getTitle()
-		.then(function(title) {
-			console.log('Title was: ' + title);
-		})
-		.end();
-} catch(e) {
-	console.log(e);
-}
+var options = { desiredCapabilities: { browserName: 'chrome' } };
+var client = webdriverio.remote(options);
+
+client
+	.init()
+	.url('https://duckduckgo.com/')
+	.setValue('#search_form_input_homepage', 'WebdriverIO')
+	.click('#search_button_homepage')
+	.getTitle().then(function(title) {
+		console.log('Title is: ' + title);
+		// outputs: "Title is: WebdriverIO (Software) at DuckDuckGo"
+	})
+	.end();
