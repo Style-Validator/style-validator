5bd07d04cc48cad4243adeeb3f8c0bb106ff5e31
igari <igari.takeharu@icloud.com>
2016-06-08 08:58:36 +0900

    commit for testing selenium on amazon linux

--- webdriverio.test.js
+++ webdriverio.test.js
@@ -4,10 +4,9 @@ var client = webdriverio.remote(options);
 
 client
 	.init()
-	.url('https://duckduckgo.com/')
-	.setValue('#search_form_input_homepage', 'WebdriverIO')
-	.click('#search_button_homepage')
-	.getTitle().then(function(title) {
+	.url('https://www.google.com/')
+	.getTitle()
+	.then(function(title) {
 		console.log('Title is: ' + title);
 		// outputs: "Title is: WebdriverIO (Software) at DuckDuckGo"
 	})
