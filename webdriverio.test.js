4dcedc82d3d9fc14f583cf0e744b4743f020e485
igari <igari.takeharu@icloud.com>
2016-06-09 23:22:08 +0900

    commit for testing selenium on amazon linux

--- webdriverio.test.js
+++ webdriverio.test.js
@@ -1,53 +1,22 @@
-var Xvfb = require('xvfb');
-var selenium = require('selenium-standalone');
-var xvfb = new Xvfb({
-	'_xvfb_args': '-screen 0, 1366x768x24'
-});
-
-xvfb.startSync();
-
+var webdriverio = require('webdriverio');
 var options = {
-	version: '2.53.0',
-	baseURL: 'https://selenium-release.storage.googleapis.com',
-	drivers: {
-		chrome: {
-			version: '2.22',
-			arch: process.arch,
-			baseURL: 'https://chromedriver.storage.googleapis.com'
+	desiredCapabilities: {
+		browserName: 'chrome',
+		chromeOptions: {
+			'binary': '/usr/bin/google-chrome'
 		}
+
 	}
 };
+var client = webdriverio.remote(options);
 
-selenium.install(options, function() {
-	console.log('Selenium is installed.');
-
-	selenium.start(options, function() {
-
-		console.log('Selenium is running.');
-
-		var webdriverio = require('webdriverio');
-		var options = {
-			desiredCapabilities: {
-				browserName: 'chrome',
-				chromeOptions: {
-					'binary': '/usr/bin/google-chrome'
-				}
-
-			}
-		};
-		var client = webdriverio.remote(options);
-
-		client
-			.init()
-			.url('https://www.google.com/')
-			.getTitle()
-			.then(function (title) {
-				console.log('Title is: ' + title);
-				// outputs: "Title is: WebdriverIO (Software) at DuckDuckGo"
-				Xvfb.stopSync();
-			})
-			.end();
+client
+	.init()
+	.url('https://www.google.com/')
+	.getTitle()
+	.then(function (title) {
+		console.log('Title is: ' + title);
+		// outputs: "Title is: WebdriverIO (Software) at DuckDuckGo"
+		Xvfb.stopSync();
 	})
-});
-
-
+	.end();
