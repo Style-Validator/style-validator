0f197509605694807a890dc6399a620589dc4ee8
igari <igari.takeharu@icloud.com>
2016-06-09 23:10:05 +0900

    commit for testing selenium on amazon linux

--- webdriverio.test.js
+++ webdriverio.test.js
@@ -1,35 +1,32 @@
 var Xvfb = require('xvfb');
-var xvfb = new Xvfb();
-xvfb.startSync();
-
 var selenium = require('selenium-standalone');
 
-selenium.install(function() {
-	console.log('Selenium is installed.');
-	selenium.start(function() {
-		console.log('Selenium is running.');
+selenium.start(function() {
+	console.log('Selenium is running.');
 
-		var webdriverio = require('webdriverio');
-		var options = { desiredCapabilities: {
-			browserName: 'chrome',
-			chromeOptions: {
-				'binary': '/usr/bin/google-chrome'
-			}
+	var xvfb = new Xvfb();
+	xvfb.startSync();
 
-		} };
-		var client = webdriverio.remote(options);
+	var webdriverio = require('webdriverio');
+	var options = { desiredCapabilities: {
+		browserName: 'chrome',
+		chromeOptions: {
+			'binary': '/usr/bin/google-chrome'
+		}
 
-		client
-			.init()
-			.url('https://www.google.com/')
-			.getTitle()
-			.then(function(title) {
-				console.log('Title is: ' + title);
-				// outputs: "Title is: WebdriverIO (Software) at DuckDuckGo"
-				Xvfb.stopSync();
-			})
-			.end();
-	});
+	} };
+	var client = webdriverio.remote(options);
+
+	client
+		.init()
+		.url('https://www.google.com/')
+		.getTitle()
+		.then(function(title) {
+			console.log('Title is: ' + title);
+			// outputs: "Title is: WebdriverIO (Software) at DuckDuckGo"
+			Xvfb.stopSync();
+		})
+		.end();
 });
 
 
