bdf1a36c277162dbb283ab67f1e38a3c3a676a1c
igari <igari.takeharu@icloud.com>
2016-06-12 15:48:34 +0900

    fixed that let the path be http not https

--- app.js
+++ app.js
@@ -190,7 +190,7 @@ function validateWithSelenium(req, res, path, targetURL) {
 
 			"var callback = arguments[arguments.length - 1];" +
 			"var sv = document.createElement('script');" +
-			"sv.src = '//style-validator.io/extension/style-validator.js?mode=manual';" +
+			"sv.src = 'http://style-validator.io/extension/style-validator.js?mode=manual';" +
 			"sv.addEventListener('load', function() {" +
 				"console.groupEnd();" +
 				"console.group('Style Validator: Executed by ' + STYLEV.caller + '.');" +
