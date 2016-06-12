9b89a41164c613d7cdb5a129896368609bd89516
igari <igari.takeharu@icloud.com>
2016-06-12 09:53:31 +0900

    added shell scripts

---
+++ docs/selenium.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Note that this script requires you to have
+# an X window running on Display :90
+# This can be done by running: /usr/bin/Xvfb :99 -ac -screen 0 1024x768x8 &
+#
+# You can save this script as /etc/init.d/selenium to start and stop selenium
+
+PORT=4444
+
+DESC="Selenium server"
+RUN_AS=selenium
+JAVA_BIN=/usr/bin/java
+
+PID_FILE="/var/run/selenium.pid"
+JAR_FILE="/var/lib/selenium-server-standalone-2.53.0.jar"
+LOG_FILE="/var/log/selenium"
+CHROME_DRIVER="/usr/bin/chromedriver"
+
+DAEMON_OPTS=" -jar $JAR_FILE -Dwebdriver.chrome.driver=$CHROME_DRIVER -log $LOG_FILE -port $PORT"
+
+NAME=selenium
+
+export DISPLAY=:99
+
+case "$1" in
+    start)
+        echo -n "Starting $DESC: "
+        start-stop-daemon --make-pidfile --pidfile /var/run/selenium.pid --start --background --exec /usr/bin/java -- -jar /var/lib/selenium/selenium-server-standalone-2.53.0.jar -Dwebdriver.chrome.driver=/usr/bin/chromedriver
+        echo "$NAME."
+        ;;
+
+    stop)
+        echo -n "Stopping $DESC: "
+        start-stop-daemon --stop --pidfile $PID_FILE
+        echo "$NAME."
+        ;;
+
+    restart|force-reload)
+        echo -n "Restarting $DESC: "
+        start-stop-daemon --stop --pidfile $PID_FILE
+        sleep 1
+        start-stop-daemon --make-pidfile --pidfile /var/run/selenium.pid --start --background --exec /usr/bin/java -- -jar /var/lib/selenium/selenium-server-standalone-2.53.0.jar -Dwebdriver.chrome.driver=/usr/bin/chromedriver
+        echo "$NAME."
+        ;;
+
+    *)
+        N=/etc/init.d/$NAME
+        echo "Usage: $N {start|stop|restart|force-reload}" >&2
+        exit 1
+        ;;
+esac
---
+++ docs/xvfb.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+XVFB=/usr/bin/Xvfb
+XVFBARGS=":99 -screen 0, 1366x768x24 -nolisten tcp"
+PIDFILE=/var/run/xvfb.pid
+
+case "$1" in
+
+  start)
+    echo -n "Starting virtual X frame buffer: Xvfb"
+    start-stop-daemon --start --quiet --pidfile $PIDFILE --make-pidfile --background --exec $XVFB -- $XVFBARGS
+    echo "."
+    ;;
+
+  stop)
+    echo -n "Stopping virtual X frame buffer: Xvfb"
+    read pid <$PIDFILE
+    pkill -TERM -P $pid
+    echo "."
+    ;;
+
+  restart)
+    $0 stop
+    $0 start
+    ;;
+
+  *)
+        echo "Usage: /etc/init.d/xvfb {start|stop|restart}"
+        exit 1
+esac
+
+exit 0
