9bad01cfd3d62af71ca43eccc04ca47215c54287
igari <igari.takeharu@icloud.com>
2016-06-12 09:53:01 +0900

    removed unused test files

--- headless.test.js
+++
@@ -1,8 +0,0 @@
-var headless = require('headless');
-
-headless(function(err, childProcess, servernum) {
-	// childProcess is a ChildProcess, as returned from child_process.spawn()
-	console.log('Xvfb running on server number', servernum);
-	console.log('Xvfb pid', childProcess.pid);
-	console.log('err should be null', err);
-});
--- xvfb.test2.js
+++
@@ -1,9 +0,0 @@
-var Xvfb = require('xvfb');
-var xvfb = new Xvfb();
-xvfb.start(function(err, xvfbProcess) {
-	console.log('err: ' + err);
-	console.log('xvfbProcess: ' + xvfbProcess);
-	xvfb.stop(function(err) {
-		console.error('err: ' + err);
-	});
-});
