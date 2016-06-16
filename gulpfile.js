ea38c9280a3816ad5edf771cfca884f9ed4b0049
igari <igari.takeharu@icloud.com>
2016-06-17 08:47:28 +0900

    added kill command

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,57 @@
-var buffer = require('buffer');
-var del = require('del');
-var map = require('map-stream');
-
+var buffer			= require('buffer');
+var del				= require('del');
+var map				= require('map-stream');
 var gulp			= require('gulp');
 var gulpUglify		= require('gulp-uglify');
 var postcss			= require('gulp-postcss');
 var sourcemaps		= require('gulp-sourcemaps');
-var autoprefixer	= require('autoprefixer');
+var gulpAutoprefixer= require('autoprefixer');
 
-gulp.task('clean', function() {
-	return del(['./dest']);
+gulp.task('clean', clean);
+gulp.task('copy', ['clean'], copy);
+gulp.task('autoprefixer', ['copy'], autoprefixer);
+gulp.task('bookmarklet', ['autoprefixer'], bookmarklet);
+gulp.task('build', ['bookmarklet']);
+gulp.task('live', function() {
+	return Promise.resolve()
+		.then(copy.bind(null, '!./src/**/app.js'))
+		.then(autoprefixer)
+		.then(bookmarklet)
 });
-
-gulp.task('copy', ['clean'], function(){
-	return gulp.src(['./src/**/*', './src/**/.*'])
-		.pipe(gulp.dest('./dest'))
+gulp.task('watch',function(){
+	gulp.watch('./src/**/*', ['live']);
 });
+gulp.task('w', ['watch']);
+gulp.task('default', ['build', 'watch']);
 
-gulp.task('autoprefixer', ['copy'], function () {
-
+function autoprefixer() {
 	return gulp.src('./src/**/*.css')
 		.pipe(sourcemaps.init())
-		.pipe(postcss([ autoprefixer({ browsers: ['last 3 versions'] }) ]))
+		.pipe(postcss([ gulpAutoprefixer({ browsers: ['last 3 versions'] }) ]))
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('./dest'));
-});
+}
 
-gulp.task('bookmarklet', ['autoprefixer'], function () {
+function bookmarklet() {
 	var header = new Buffer('javascript:void ');
-	return gulp.src('src/bookmarklet/style-validator.js')
+	return gulp.src('./src/bookmarklet/style-validator.js')
 		.pipe(gulpUglify())
 		.pipe(map(function (file, cb) {
 			file.contents = buffer.Buffer.concat([header, file.contents]);
 			cb(null, file);
 		}))
 		.pipe(gulp.dest('dest/bookmarklet'));
-});
-gulp.task('build', ['bookmarklet']);
+}
 
-gulp.task('watch',function(){
-	gulp.watch('./src/**/*', ['build']);
-});
+function copy(exceptionSrc) {
+	var targetSrc = ['./src/**/*', './src/**/.*'];
+	if(typeof exceptionSrc === 'string') {
+		targetSrc.push(exceptionSrc);
+	}
+	return gulp.src(targetSrc)
+		.pipe(gulp.dest('./dest'));
+}
 
-gulp.task('default', ['build', 'watch']);
-
+function clean() {
+	return del(['./dest']);
+}
